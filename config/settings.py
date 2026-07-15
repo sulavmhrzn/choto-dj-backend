aee79d8de6bcc45c1829d6af5998d8bbbd4c8094
Sulav Maharjan <sulavmaharjan63@gmail.com>
2026-07-15 11:54:27 +0545

    ci: add Github Actions test workflow

--- config/settings.py
+++ config/settings.py
@@ -171,7 +171,7 @@ CELERY_RESULT_SERIALIZER = "json"
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": os.getenv("REDIS_URL", default="redis://redis:6379/1"),
+        "LOCATION": os.getenv("REDIS_CACHE_URL", default="redis://redis:6379/1"),
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
             "IGNORE_EXCEPTIONS": True,
