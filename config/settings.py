6a39d43bc02a452b308ff1d47540d3ec54019dc9
Sulav Maharjan <sulavmaharjan63@gmail.com>
2026-09-04 21:19:47 +0545

    fix: add default CSRF origins

--- config/settings.py
+++ config/settings.py
@@ -14,7 +14,9 @@ DEBUG = os.getenv("DEBUG", "True") == "True"
 
 
 ALLOWED_HOSTS = ["*"]
-CSRF_TRUSTED_ORIGINS = os.getenv("CSRF_TRUSTED_ORIGINS").split(",")
+CSRF_TRUSTED_ORIGINS = os.getenv(
+    "CSRF_TRUSTED_ORIGINS", "http://localhost,http://127.0.0.1"
+).split(",")
 
 
 INSTALLED_APPS = [
